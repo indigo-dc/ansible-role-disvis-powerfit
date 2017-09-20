c358c101f246e5dc16d75be7dbbf8bccb52c3989
orviz <orviz@ifca.unican.es>
2017-09-20 16:49:28 +0200

    Use udocker to run disvis

--- dockers/docker-disvis/jenkins/run_disvis.sh
+++ dockers/docker-disvis/jenkins/run_disvis.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-disvis input_files/fixed_chain.pdb input_files/scanning_chain.pdb input_files/restraints.txt -a 60.00 -vs 4.00 -d results
+git clone https://github.com/indigo-dc/udocker
+cd udocker
+./udocker.py pull indigodatacloudapps/disvis:jenkins-preview
+
+./udocker.py create --name=jenkins-preview indigodatacloudapps/jenkins-preview
+cp -r input_files /tmp/.
+mkdir /tmp/results
+./udocker.py run -v /dev --volume=/tmp/:/home jenkins-preview disvis /home/input_files/fixed_chain.pdb /home/input_files/scanning_chain.pdb /home/input_files/restraints.txt -a 60.00 -vs 4.00 -d /home/results
+
+./udocker.py rm jenkins-preview
+./udocker.py rmi indigodatacloudapps/disvis:jenkins-preview
