b2bde9624c5545438734c48a7579bd0ee19c659f
orviz <orviz@ifca.unican.es>
2017-09-20 16:53:35 +0200

    fix

--- dockers/docker-disvis/jenkins/run_disvis.sh
+++ dockers/docker-disvis/jenkins/run_disvis.sh
@@ -4,7 +4,7 @@ git clone https://github.com/indigo-dc/udocker
 cd udocker
 ./udocker.py pull indigodatacloudapps/disvis:jenkins-preview
 
-./udocker.py create --name=jenkins-preview indigodatacloudapps/jenkins-preview
+./udocker.py create --name=jenkins-preview indigodatacloudapps/disvis:jenkins-preview
 cp -r input_files /tmp/.
 mkdir /tmp/results
 ./udocker.py run -v /dev --volume=/tmp/:/home jenkins-preview disvis /home/input_files/fixed_chain.pdb /home/input_files/scanning_chain.pdb /home/input_files/restraints.txt -a 60.00 -vs 4.00 -d /home/results
